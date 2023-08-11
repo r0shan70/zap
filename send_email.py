3418ec3d2394b296ee89119dbf7e13037b8dd62e
r0shan70 <137083637+r0shan70@users.noreply.github.com>
2023-08-11 16:52:13 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 from botocore.exceptions import NoCredentialsError
 import os
@@ -23,18 +22,13 @@ region = 'eu-north-1'  # Change to your desired AWS region
 # Set email parameters
 sender_email = 'roshanofficial27@gmail.com'
 recipient_email = 'devopstesting539@gmail.com'
-cc_recipients = ['hellofabin@gmail.com.com', 'mathewrijo23@gmail.com']  # Add CC email addresses here
+cc_email = ['hellofabin@gmail.com', 'mathewrijo23@gmail.com']  # Add more CC recipients as needed
 subject = 'Test email with attachment'
-body_text = 'This is a email with an attachment of OWASP ZAP report.'
-body_html = '<html><body><h1>This is a email with an attachment of OWASP ZAP report.</h1></body></html>'
+body_text = 'This is a test email with an attachment sent from boto3.'
+body_html = '<html><body><h1>This is a test email with an attachment sent from boto3.</h1></body></html>'
 
-# Use an environment variable to specify the attachment file path
-attachment_file_path = os.environ.get('file.html')
-
-# Ensure the specified file exists
-if not os.path.exists(attachment_file_path):
-    print("Attachment file not found:", attachment_file_path)
-    exit(1)
+# Path to the file you want to attach
+attachment_file_path = 'file.html'
 
 # Connect to Amazon SES
 ses = boto3.client('ses', region_name=region, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key)
@@ -48,13 +42,14 @@ msg = MIMEMultipart('mixed')
 msg['Subject'] = subject
 msg['From'] = sender_email
 msg['To'] = recipient_email
+msg['Cc'] = ', '.join(cc_email)  # Adding CC recipients
 
 # Attach the text part
 msg.attach(MIMEText(body_text, 'plain'))
 msg.attach(MIMEText(body_html, 'html'))
 
 # Attach the file
-attachment_mime = MIMEApplication(attachment_content)
+attachment_mime = MIMEApplication(attachment_content, _subtype='html')
 attachment_mime.add_header('Content-Disposition', 'attachment', filename=os.path.basename(attachment_file_path))
 msg.attach(attachment_mime)
 
@@ -65,7 +60,7 @@ raw_message = msg.as_string()
 try:
     response = ses.send_raw_email(
         Source=sender_email,
-        Destinations=[recipient_email] + cc_recipients,  # Combine primary recipient and CC recipients
+        Destinations=[recipient_email],
         RawMessage={'Data': raw_message}
     )
     print("Email with attachment sent successfully!")
